dfb6e37e8dea318efdf17a0ba8027a5a36c54b14
LukeHaines <lukehaines2@gmail.com>
2015-10-26 20:50:12 +0000

    got second graph type displaying

--- public/js/graph.js
+++ public/js/graph.js
@@ -39,7 +39,24 @@ var pieData = [
 var context = document.getElementById('skills').getContext('2d');
 var skillsChart = new Chart(context).Pie(pieData);
 
-// var context = document.getElementById('skills').getContext('2d');
-// var clientsChart = new Chart(context).Bar(pieData);
+
+var barData = {
+    labels: ['Italy', 'UK', 'USA', 'Germany', 'France', 'Japan'],
+    datasets: [
+        {
+            label: '2010 customers #',
+            fillColor: '#382765',
+            data: [2500, 1902, 1041, 610, 1245, 952]
+        },
+        {
+            label: '2014 customers #',
+            fillColor: '#7BC225',
+            data: [3104, 1689, 1318, 589, 1199, 1436]
+        }
+    ]
+};
+
+var context = document.getElementById('clients').getContext('2d');
+var clientsChart = new Chart(context).Bar(barData);
 
 });
