88adf6f2eb1332bbb1a1022410d19385c8a395e0
LukeHaines <lukehaines2@gmail.com>
2015-10-28 13:32:29 +0000

    MIRACLE. Pie chart showing correct data

--- public/js/graph.js
+++ public/js/graph.js
@@ -26,68 +26,73 @@ $(document).ready(function(){
 
 
   // PIE GRAPH
-  var pieData = [
-     {
-        value: 25,
-        label: 'Java',
-        color: '#811BD6'
-     },
-     {
-        value: 10,
-        label: 'Scala',
-        color: '#9CBABA'
-     },
-     {
-        value: 30,
-        label: 'PHP',
-        color: '#D18177'
-     },
-     {
-        value : 75,
-        label: 'HTML',
-        color: '#6AE128'
-     }
-  ];
+  // var pieData = [
+  //    {
+  //       value: 25,
+  //       label: 'Java',
+  //       color: '#811BD6'
+  //    },
+  //    {
+  //       value: 10,
+  //       label: 'Scala',
+  //       color: '#9CBABA'
+  //    },
+  //    {
+  //       value: 30,
+  //       label: 'PHP',
+  //       color: '#D18177'
+  //    },
+  //    {
+  //       value : 75,
+  //       label: 'HTML',
+  //       color: '#6AE128'
+  //    }
+  // ];
 
-  var context = document.getElementById('skills').getContext('2d');
-  var skillsChart = new Chart(context).Pie(pieData);
+  // var context = document.getElementById('skills').getContext('2d');
+  // var skillsChart = new Chart(context).Pie(pieData);
 
 
-  // BAR GRAPH
-  var barData = {
-      labels: ['Italy', 'UK', 'USA', 'Germany', 'France', 'Japan'],
-      datasets: [
-          {
-              label: '2010 customers #',
-              fillColor: '#382765',
-              data: [2500, 1902, 1041, 610, 1245, 952]
-          },
-          {
-              label: '2014 customers #',
-              fillColor: '#7BC225',
-              data: [3104, 1689, 1318, 589, 1199, 1436]
-          }
-      ]
-  };
+  // // BAR GRAPH
+  // var barData = {
+  //     labels: ['Italy', 'UK', 'USA', 'Germany', 'France', 'Japan'],
+  //     datasets: [
+  //         {
+  //             label: '2010 customers #',
+  //             fillColor: '#382765',
+  //             data: [2500, 1902, 1041, 610, 1245, 952]
+  //         },
+  //         {
+  //             label: '2014 customers #',
+  //             fillColor: '#7BC225',
+  //             data: [3104, 1689, 1318, 589, 1199, 1436]
+  //         }
+  //     ]
+  // };
 
-  var context = document.getElementById('clients').getContext('2d');
-  var clientsChart = new Chart(context).Bar(barData);
+  // var context = document.getElementById('clients').getContext('2d');
+  // var clientsChart = new Chart(context).Bar(barData);
 
   //PERCENTAGE CALC
   function percentageChartLoop(){
     var percentageChart = [];
-    bigData.forEach(function(obj) { 
-      var percentage1 = (obj['%']*100);
-      var discount1 = obj['Discount']; 
-      percentageChart.push({value: percentage1, color: '#7BC225'})
-      
-      var counter = {};
-      percentageChart.forEach(function(obj) {
-        var key = JSON.stringify(obj)
-        counter[key] = (counter[key] || 0) + 1
-      })
-      console.log(counter)
-    });
+    var uniquePercentages = [];
+    var percentageObject = {};
+
+    for (var i = 0; i < bigData.length; i++) {
+      var percentage = (bigData[i]['%']*100);
+      if(!percentageObject[percentage]) percentageObject[percentage] = 0
+
+      if(uniquePercentages.indexOf(percentage) === -1) uniquePercentages.push(percentage)
+      percentageObject[percentage] += 1
+    };
+
+    for(var prop in percentageObject) {
+      if(percentageObject.hasOwnProperty(prop)) {
+        percentageChart.push({ value: percentageObject[prop], label: prop + '%', color: 'red' })
+      }
+    }
+
     return percentageChart;
   }
 
