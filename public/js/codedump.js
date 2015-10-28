3ee88923a1034caee9cfeaff69123e64c550e490
LukeHaines <lukehaines2@gmail.com>
2015-10-28 19:03:36 +0000

    basics of css implemented but the display is not working well

--- public/js/codedump.js
+++ public/js/codedump.js
@@ -24,3 +24,39 @@
         context.fillText(tpercentage, txtPosx, canvasHeight/2);
       }
   });
+
+
+
+
+
+
+/*#donut-container {
+  max-width: 400px;
+}*/
+/*
+
+
+.donut-inner
+{
+margin-top: -200px;
+margin-bottom: 100px;
+}
+.donut-inner h5
+{
+margin-bottom: 5px;
+margin-top: 0;
+}
+.donut-inner span {font-size: 18px;}*/
+
+
+
+
+
+
+
+    <!-- <div id="donut-container"> -->
+<!-- </div> -->
+    <!-- <div class="donut-inner">
+      <h5>47 / 60 st</h5>
+      <span>(30 / 25 st)</span>
+  </div> -->
