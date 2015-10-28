975585e4599cd662b2a99b4a8cf8d74aeeb7b371
LukeHaines <lukehaines2@gmail.com>
2015-10-28 19:38:03 +0000

    threw in some additional graphs

--- public/js/graph.js
+++ public/js/graph.js
@@ -13,6 +13,46 @@ $(document).ready(function(){
     })
 
 
+    var data2 = {
+    labels : ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"],
+    datasets : [
+        {
+            fillColor : "rgba(99,123,133,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [65,54,30,81,56,55,40]
+        },
+        {
+            fillColor : "rgba(219,186,52,0.4)",
+            strokeColor : "rgba(220,220,220,1)",
+            pointColor : "rgba(220,220,220,1)",
+            pointStrokeColor : "#fff",
+            data : [20,60,42,58,31,21,50]
+        },
+      ]
+    }
+    var canvas = document.getElementById("shipments");
+    var ctx = canvas.getContext("2d");
+    new Chart(ctx).Line(data2);
+
+    var data3 = {
+        labels : ["Helpful","Friendly","Kind","Rude","Slow","Frustrating"],
+        datasets : [
+            {
+                fillColor : "rgba(220,220,220,0.5)",
+                strokeColor : "#637b85",
+                pointColor : "#dbba34",
+                pointStrokeColor : "#637b85",
+                data : [65,59,90,81,30,56]
+            }
+        ]
+    }
+    var canvas = document.getElementById("departments");
+    var ctx = canvas.getContext("2d");
+    new Chart(ctx).Radar(data3);
+
+
   //PERCENTAGE CALC
   function percentageChartLoop(){
     var percentageChart = [];
